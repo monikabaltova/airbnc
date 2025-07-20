3ab8824ab27905919297e4cde3619f4232d5c15f
monikabaltova <monika.baltova31@gmail.com>
2025-07-20 19:21:54 +0100

    created table properties

--- db/data/seed.js
+++ db/data/seed.js
@@ -9,8 +9,9 @@ const {
 const formattedData = require("./utils/formatedData.js");
 
 async function seed() {
-  await db.query(`DROP TABLE IF EXISTS property_types`);
+  await db.query(`DROP TABLE IF EXISTS properties`);
   await db.query(`DROP TABLE IF EXISTS users`);
+  await db.query(`DROP TABLE IF EXISTS property_types`);
 
   await db.query(`CREATE TABLE property_types(
     property_type VARCHAR NOT NULL PRIMARY KEY,
@@ -28,7 +29,7 @@ async function seed() {
   );
 
   await db.query(`CREATE TABLE users(
-      user_id SERIAL,
+      user_id SERIAL PRIMARY KEY,
       first_name VARCHAR NOT NULL,
       surname VARCHAR NOT NULL,
       email VARCHAR NOT NULL,
@@ -49,6 +50,32 @@ async function seed() {
       formattedUsersData
     )
   );
+
+  await db.query(`CREATE TABLE properties(
+   property_id SERIAL PRIMARY KEY,
+   host_id INT NOT NULL REFERENCES users(user_id),
+   name VARCHAR NOT NULL,
+   location VARCHAR NOT NULL,
+   property_type VARCHAR NOT NULL REFERENCES property_types(property_type),
+   price_per_night FLOAT(2) NOT NULL,
+   description TEXT
+   )`);
+
+  const formattedPropertiesData = formattedData(propertiesData);
+  await db.query(
+    format(
+      `INSERT INTO properties(
+   name,
+   property_type,
+     location,
+   price_per_night,
+   description,
+   
+      ) VALUES %L`,
+      formattedPropertiesData
+    )
+  );
+  console.log("working");
 }
 
 module.exports = seed;
