7bcc51f4f9921ee4e47e0dcf1dc34bd2be51acc1
monikabaltova <monika.baltova31@gmail.com>
2025-08-12 17:50:04 +0100

    update connection file

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,11 @@ const ENV = process.env.NODE_ENV || "dev";
 
 console.log(ENV);
 
-require("dotenv").config({ path: `${__dirname}/../.evn.${ENV}` });
+require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
 
 const config = {};
 
