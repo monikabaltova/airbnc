5f68a61a8cf1676f05a8006ba5a7fa6a6050c760
monikabaltova <monika.baltova31@gmail.com>
2025-08-13 19:53:50 +0100

    update error controller

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -2,21 +2,19 @@ exports.handlePathNotFound = (req, res, next) => {
   res.status(404).send({ msg: "Path not found." });
 };
 
-exports.handleCustomError = (err, req, res, next) => {
-  if (err.status && err.msg) {
-    res.status(err.status).send({ msg: err.msg });
+exports.handleBadRequest = (err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad request: ivalid data" });
+  } else if (err.code === "23502") {
+    res.status(400).send({ msg: "Fill up all required fields" });
   } else {
     next(err);
   }
 };
 
-exports.handleDataNotFound = (err, req, res, next) => {
-  res.status(404).send({ msg: "Data not found." });
-};
-
-exports.handleBadRequest = (err, req, res, next) => {
-  if (err.code) {
-    res.status(400).send({ msg: "Bad request." });
+exports.handleCustomError = (err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
   } else {
     next(err);
   }
